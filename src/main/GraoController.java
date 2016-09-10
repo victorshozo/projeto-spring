bf122af322a7dd9756e39454e762aba5d18553eb
Robles <carolina.robles@accenture.com>
2016-09-09 21:42:36 -0300

    Renomeia de grao para graos

--- src/main/GraoController.java
+++ src/main/GraoController.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GraoController {
 
-	@RequestMapping("/grao")
-	public ArrayList<Grao> grao() {
-		return getGraos();
-	}
-
-	public ArrayList<Grao> getGraos() {
-		ArrayList<Grao> graos = new ArrayList<>();
-	       graos.add(new Grao(1, "Milho", 30));
-	        graos.add(new Grao(2, "Soja", 45));
-	        graos.add(new Grao(3, "Sordo", 75));
+	@RequestMapping("/graos")
+	public List<Grao> grao() {
+		List<Grao> graos = new ArrayList<>();
+		graos.add(new Grao(1, "Milho", 30));
+		graos.add(new Grao(2, "Soja", 45));
+		graos.add(new Grao(3, "Sordo", 75));
 
 		return graos;
 	}
+
 }
