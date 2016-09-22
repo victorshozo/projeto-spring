1f8679ee17f5d2377883ca33106be91b6e80e422
double <carolina.robles95@yahoo.com.br>
2016-09-21 21:49:17 -0300

    configurou datasource

--- src/main/java/com/graincare/configuration/JPAConfiguration.java
+++ src/main/java/com/graincare/configuration/JPAConfiguration.java
@@ -1,7 +1,13 @@
 package com.graincare.configuration;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -9,4 +15,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = { "com.graincare" })
 public class JPAConfiguration {
 
+	@Resource
+	private Environment env;
+
+	@Bean
+	DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+		dataSource.setUrl(env.getRequiredProperty("db.url"));
+		dataSource.setUsername(env.getRequiredProperty("db.username"));
+		dataSource.setPassword(env.getRequiredProperty("db.password"));
+
+		return dataSource;
+	}
 }
