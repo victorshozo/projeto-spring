37a14bcd56c4b13d2a226a3f35d16bd2fc815dff
Shozomon <victor_shozo@hotmail.com>
2016-10-17 21:42:34 -0200

    Adicionado valores temperature ao GrainType

--- src/main/java/com/graincare/graos/GrainType.java
+++ src/main/java/com/graincare/graos/GrainType.java
@@ -1,7 +1,16 @@
 package com.graincare.graos;
 
 public enum GrainType {
-	MILHO,
-	SOJA
+	MILHO(36),
+	SOJA(55)
 	;
+private Double maxTemperature;
+
+GrainType(Double maxTemperature){
+	this.maxTemperature = maxTemperature;
+}
+
+public Double getMaxTemperature(){
+	return this.maxTemperature;
+}
 }
